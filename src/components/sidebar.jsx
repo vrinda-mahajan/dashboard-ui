6c248390b376cea72436c3da7a55fb19aee1e0cd
vrinda-mahajan <vrindamahajan96@gmail.com>
2024-01-08 22:47:42 +0530

    initial setup

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,16 @@
+const sidebarItemsData = [
+    { itemName: "Home", itemIcon: Category },
+    { itemName: "Messages", itemIcon: Message },
+    { itemName: "Tasks", itemIcon: TaskSquare },
+    { itemName: "Members", itemIcon: Share },
+    { itemName: "Settings", itemIcon: Setting },
+  ];
+function Sidebar() {
+    return (
+      <div className=''>
+        <div>
+            <img src="" alt="logo" />
+        </div>
+      </div>
+    );
+  }
